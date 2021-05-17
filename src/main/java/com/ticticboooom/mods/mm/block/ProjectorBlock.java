159db445c758b248cd47289335ec1701c4790f67
Ridanis <khaled.chikhou@gmail.com>
2021-05-17 09:52:25 +0200

    Added VoxelShapes for the Projector and StrctureGen

--- src/main/java/com/ticticboooom/mods/mm/block/ProjectorBlock.java
+++ src/main/java/com/ticticboooom/mods/mm/block/ProjectorBlock.java
@@ -10,6 +10,7 @@ import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -21,49 +22,108 @@ import javax.annotation.Nullable;
 import java.util.stream.Stream;
 
 public class ProjectorBlock extends Block {
+    private static final DirectionProperty FACING = HorizontalBlock.FACING;
+
+    private static final VoxelShape SHAPE_N = Stream.of(
+            Block.box(4, 2, 4, 12, 3, 12),
+            Block.box(4, 0, 4, 12, 1, 12),
+            Block.box(6, 2.5, 6, 10, 3.5, 10),
+            Block.box(5, 1, 5, 11, 2, 11),
+            Block.box(5, 6, 5, 11, 12, 11),
+            Block.box(0.75, 2.5, 6.75, 2.25, 4.75, 9.25),
+            Block.box(13.75, 2.5, 6.75, 15.25, 4.75, 9.25),
+            Block.box(6, 3.5, 14, 7, 12.75, 15),
+            Block.box(5.75, 11.25, 13.75, 7.25, 13.5, 15.25),
+            Block.box(5.75, 2.5, 13.75, 7.25, 4.75, 15.25),
+            Block.box(8.75, 11.25, 13.75, 10.25, 13.5, 15.25),
+            Block.box(8.75, 2.5, 13.75, 10.25, 4.75, 15.25),
+            Block.box(9, 3.5, 14, 10, 12.75, 15),
+            Block.box(1.25, 6.25, 7.5, 2.25, 7.25, 8.5),
+            Block.box(13.75, 6.25, 7.5, 14.75, 7.25, 8.5),
+            Block.box(14, 3.5, 7, 15, 7.75, 9),
+            Block.box(1, 3.5, 7, 2, 7.75, 9),
+            Block.box(11, 2, 6, 15, 4, 10),
+            Block.box(1, 2, 6, 5, 4, 10),
+            Block.box(6, 2, 11, 10, 4, 15),
+            Block.box(6, 12, 9, 10, 15, 15)
+    ).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
+
+    private static final VoxelShape SHAPE_E = Stream.of(
+            Block.box(4, 2, 4, 12, 3, 12),
+            Block.box(4, 0, 4, 12, 1, 12),
+            Block.box(6, 2.5, 6, 10, 3.5, 10),
+            Block.box(5, 1, 5, 11, 2, 11),
+            Block.box(5, 6, 5, 11, 12, 11),
+            Block.box(6.75, 2.5, 0.75, 9.25, 4.75, 2.25),
+            Block.box(6.75, 2.5, 13.75, 9.25, 4.75, 15.25),
+            Block.box(1, 3.5, 6, 2, 12.75, 7),
+            Block.box(0.75, 11.25, 5.75, 2.25, 13.5, 7.25),
+            Block.box(0.75, 2.5, 5.75, 2.25, 4.75, 7.25),
+            Block.box(0.75, 11.25, 8.75, 2.25, 13.5, 10.25),
+            Block.box(0.75, 2.5, 8.75, 2.25, 4.75, 10.25),
+            Block.box(1, 3.5, 9, 2, 12.75, 10),
+            Block.box(7.5, 6.25, 1.25, 8.5, 7.25, 2.25),
+            Block.box(7.5, 6.25, 13.75, 8.5, 7.25, 14.75),
+            Block.box(7, 3.5, 14, 9, 7.75, 15),
+            Block.box(7, 3.5, 1, 9, 7.75, 2),
+            Block.box(6, 2, 11, 10, 4, 15),
+            Block.box(6, 2, 1, 10, 4, 5),
+            Block.box(1, 2, 6, 5, 4, 10),
+            Block.box(1, 12, 6, 7, 15, 10)
+    ).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
+
+    private static final VoxelShape SHAPE_S = Stream.of(
+            Block.box(4, 2, 4, 12, 3, 12),
+            Block.box(4, 0, 4, 12, 1, 12),
+            Block.box(6, 2.5, 6, 10, 3.5, 10),
+            Block.box(5, 1, 5, 11, 2, 11),
+            Block.box(5, 6, 5, 11, 12, 11),
+            Block.box(13.75, 2.5, 6.75, 15.25, 4.75, 9.25),
+            Block.box(0.75, 2.5, 6.75, 2.25, 4.75, 9.25),
+            Block.box(9, 3.5, 1, 10, 12.75, 2),
+            Block.box(8.75, 11.25, 0.75, 10.25, 13.5, 2.25),
+            Block.box(8.75, 2.5, 0.75, 10.25, 4.75, 2.25),
+            Block.box(5.75, 11.25, 0.75, 7.25, 13.5, 2.25),
+            Block.box(5.75, 2.5, 0.75, 7.25, 4.75, 2.25),
+            Block.box(6, 3.5, 1, 7, 12.75, 2),
+            Block.box(13.75, 6.25, 7.5, 14.75, 7.25, 8.5),
+            Block.box(1.25, 6.25, 7.5, 2.25, 7.25, 8.5),
+            Block.box(1, 3.5, 7, 2, 7.75, 9),
+            Block.box(14, 3.5, 7, 15, 7.75, 9),
+            Block.box(1, 2, 6, 5, 4, 10),
+            Block.box(11, 2, 6, 15, 4, 10),
+            Block.box(6, 2, 1, 10, 4, 5),
+            Block.box(6, 12, 1, 10, 15, 7)
+    ).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
+
+    private static final VoxelShape SHAPE_W = Stream.of(
+            Block.box(4, 2, 4, 12, 3, 12),
+            Block.box(4, 0, 4, 12, 1, 12),
+            Block.box(6, 2.5, 6, 10, 3.5, 10),
+            Block.box(5, 1, 5, 11, 2, 11),
+            Block.box(5, 6, 5, 11, 12, 11),
+            Block.box(6.75, 2.5, 13.75, 9.25, 4.75, 15.25),
+            Block.box(6.75, 2.5, 0.75, 9.25, 4.75, 2.25),
+            Block.box(14, 3.5, 9, 15, 12.75, 10),
+            Block.box(13.75, 11.25, 8.75, 15.25, 13.5, 10.25),
+            Block.box(13.75, 2.5, 8.75, 15.25, 4.75, 10.25),
+            Block.box(13.75, 11.25, 5.75, 15.25, 13.5, 7.25),
+            Block.box(13.75, 2.5, 5.75, 15.25, 4.75, 7.25),
+            Block.box(14, 3.5, 6, 15, 12.75, 7),
+            Block.box(7.5, 6.25, 13.75, 8.5, 7.25, 14.75),
+            Block.box(7.5, 6.25, 1.25, 8.5, 7.25, 2.25),
+            Block.box(7, 3.5, 1, 9, 7.75, 2),
+            Block.box(7, 3.5, 14, 9, 7.75, 15),
+            Block.box(6, 2, 1, 10, 4, 5),
+            Block.box(6, 2, 11, 10, 4, 15),
+            Block.box(11, 2, 6, 15, 4, 10),
+            Block.box(9, 12, 6, 15, 15, 10)
+    ).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
+
     public ProjectorBlock() {
         super(Properties.of(Material.METAL));
         this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH));
     }
-
-    private static final DirectionProperty FACING = HorizontalBlock.FACING;
-
-    private static final VoxelShape SHAPE_N = Stream.of(
-            Block.box(0, 0, 0, 16, 8, 16),
-            Block.box(0, 8, 0, 4, 10, 16),
-            Block.box(4, 8, 8, 16, 12, 16),
-            Block.box(12, 8, 0, 16, 10, 8),
-            Block.box(5, 8, 1, 11, 12, 7),
-            Block.box(5, 12, 9, 11, 16, 15)
-    ).reduce((v1, v2) -> VoxelShapes.join(v1, v2, IBooleanFunction.OR)).get();
-
-    private static final VoxelShape SHAPE_E = Stream.of(
-            Block.box(0, 0, 0, 16, 8, 16),
-            Block.box(0, 8, 0, 16, 10, 4),
-            Block.box(0, 8, 4, 8, 12, 16),
-            Block.box(8, 8, 12, 16, 10, 16),
-            Block.box(9, 8, 5, 15, 12, 11),
-            Block.box(1, 12, 5, 7, 16, 11)
-    ).reduce((v1, v2) -> VoxelShapes.join(v1, v2, IBooleanFunction.OR)).get();
-
-    private static final VoxelShape SHAPE_S = Stream.of(
-            Block.box(0, 0, 0, 16, 8, 16),
-            Block.box(12, 8, 0, 16, 10, 16),
-            Block.box(0, 8, 0, 12, 12, 8),
-            Block.box(0, 8, 8, 4, 10, 16),
-            Block.box(5, 8, 9, 11, 12, 15),
-            Block.box(5, 12, 1, 11, 16, 7)
-    ).reduce((v1, v2) -> VoxelShapes.join(v1, v2, IBooleanFunction.OR)).get();
-
-    private static final VoxelShape SHAPE_W = Stream.of(
-            Block.box(0, 0, 0, 16, 8, 16),
-            Block.box(0, 8, 12, 16, 10, 16),
-            Block.box(8, 8, 0, 16, 12, 12),
-            Block.box(0, 8, 0, 8, 10, 4),
-            Block.box(1, 8, 5, 7, 12, 11),
-            Block.box(9, 12, 5, 15, 16, 11)
-    ).reduce((v1, v2) -> VoxelShapes.join(v1, v2, IBooleanFunction.OR)).get();
-
 
     @Override
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
--- src/main/java/com/ticticboooom/mods/mm/block/StructureGenBlock.java
+++ src/main/java/com/ticticboooom/mods/mm/block/StructureGenBlock.java
@@ -5,23 +5,133 @@ import com.ticticboooom.mods.mm.block.tile.StructureGenBlockEntity;
 import com.ticticboooom.mods.mm.registration.MMSetup;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
 import javax.annotation.Nullable;
+import java.util.stream.Stream;
 
 public class StructureGenBlock extends Block {
+    private static final DirectionProperty FACING = HorizontalBlock.FACING;
+
+    private static final VoxelShape SHAPE_N = Stream.of(
+            Block.box(1, 2, 1, 15, 5, 6),
+            Block.box(0, 0, 0, 16, 2, 16),
+            Block.box(0.5, 2, 6.5, 15.5, 5, 15.5),
+            Block.box(0, 5, 6, 16, 6, 16),
+            Block.box(4, 6, 7, 15, 14, 13),
+            Block.box(1, 5.5, 7, 3, 6.5, 9),
+            Block.box(1.25, 11, 7.25, 2.75, 11.25, 8.75),
+            Block.box(1, 6, 11, 3, 12.25, 14),
+            Block.box(1, 11.25, 6.75, 3, 12.25, 11.25),
+            Block.box(0, 2, 0, 1, 3, 6),
+            Block.box(15, 2, 0, 16, 3, 6),
+            Block.box(0, 2, 6, 1, 5, 7),
+            Block.box(15, 2, 6, 16, 5, 7),
+            Block.box(15, 2, 15, 16, 5, 16),
+            Block.box(0, 2, 15, 1, 5, 16)
+    ).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
+
+    private static final VoxelShape SHAPE_E = Stream.of(
+            Block.box(10, 2, 1, 15, 5, 15),
+            Block.box(0, 0, 0, 16, 2, 16),
+            Block.box(0.5, 2, 0.5, 9.5, 5, 15.5),
+            Block.box(0, 5, 0, 10, 6, 16),
+            Block.box(3, 6, 4, 9, 14, 15),
+            Block.box(7, 5.5, 1, 9, 6.5, 3),
+            Block.box(7.25, 11, 1.25, 8.75, 11.25, 2.75),
+            Block.box(2, 6, 1, 5, 12.25, 3),
+            Block.box(4.75, 11.25, 1, 9.25, 12.25, 3),
+            Block.box(10, 2, 0, 16, 3, 1),
+            Block.box(10, 2, 15, 16, 3, 16),
+            Block.box(9, 2, 0, 10, 5, 1),
+            Block.box(9, 2, 15, 10, 5, 16),
+            Block.box(0, 2, 15, 1, 5, 16),
+            Block.box(0, 2, 0, 1, 5, 1)
+    ).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
+
+    private static final VoxelShape SHAPE_S = Stream.of(
+            Block.box(1, 2, 10, 15, 5, 15),
+            Block.box(0, 0, 0, 16, 2, 16),
+            Block.box(0.5, 2, 0.5, 15.5, 5, 9.5),
+            Block.box(0, 5, 0, 16, 6, 10),
+            Block.box(1, 6, 3, 12, 14, 9),
+            Block.box(13, 5.5, 7, 15, 6.5, 9),
+            Block.box(13.25, 11, 7.25, 14.75, 11.25, 8.75),
+            Block.box(13, 6, 2, 15, 12.25, 5),
+            Block.box(13, 11.25, 4.75, 15, 12.25, 9.25),
+            Block.box(15, 2, 10, 16, 3, 16),
+            Block.box(0, 2, 10, 1, 3, 16),
+            Block.box(15, 2, 9, 16, 5, 10),
+            Block.box(0, 2, 9, 1, 5, 10),
+            Block.box(0, 2, 0, 1, 5, 1),
+            Block.box(15, 2, 0, 16, 5, 1)
+    ).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
+
+    private static final VoxelShape SHAPE_W = Stream.of(
+            Block.box(1, 2, 1, 6, 5, 15),
+            Block.box(0, 0, 0, 16, 2, 16),
+            Block.box(6.5, 2, 0.5, 15.5, 5, 15.5),
+            Block.box(6, 5, 0, 16, 6, 16),
+            Block.box(7, 6, 1, 13, 14, 12),
+            Block.box(7, 5.5, 13, 9, 6.5, 15),
+            Block.box(7.25, 11, 13.25, 8.75, 11.25, 14.75),
+            Block.box(11, 6, 13, 14, 12.25, 15),
+            Block.box(6.75, 11.25, 13, 11.25, 12.25, 15),
+            Block.box(0, 2, 15, 6, 3, 16),
+            Block.box(0, 2, 0, 6, 3, 1),
+            Block.box(6, 2, 15, 7, 5, 16),
+            Block.box(6, 2, 0, 7, 5, 1),
+            Block.box(15, 2, 0, 16, 5, 1),
+            Block.box(15, 2, 15, 16, 5, 16)
+    ).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
+
     public StructureGenBlock() {
         super(Properties.of(Material.METAL));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder.add(FACING));
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext ctx) {
+        return this.defaultBlockState().setValue(FACING, ctx.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_) {
+        switch (state.getValue(FACING)) {
+            case NORTH:
+                return SHAPE_N;
+            case EAST:
+                return SHAPE_E;
+            case SOUTH:
+                return SHAPE_S;
+            case WEST:
+                return SHAPE_W;
+            default:
+                throw new IllegalStateException("Invalid State");
+        }
     }
 
     @Override
